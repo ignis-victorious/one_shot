be0ccded86260ec03da3f7ca2a419b768b30bfd5
Emmanuele Levi <ignisvictorious@gmail.com>
2025-05-25 16:28:25 +0100

    add buttpn to clear screen but can be made better

--- src/main.py
+++ src/main.py
@@ -1,63 +1,55 @@
 #  _______________________
 #  Import LIBRARIES
-import re
 import flet as ft
-from flet import Page, app, Text, TextField
+from flet import Page, app, Text, TextField, Column, ElevatedButton
 #  Import FILES
 #  https://www.youtube.com/watch?v=S64XGQiQp68
 #  https://www.youtube.com/watch?v=DHUl_KncLdU
 #  https://www.youtube.com/watch?v=xr7vDSFXjW0
 #  https://www.youtube.com/watch?v=529LYDgRTgQ
 
-# _________________ @ 2:12:58
+# _________________ @ 2:31:20
 
 
 def main(page: Page) -> None:
-    page.title = "Password strength checker"
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.padding = 20
+    page.title = "Multiplication-table Generator"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.padding = 30
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    result_text: Text = Text(value="", size=20, weight=ft.FontWeight.BOLD)
+    tmp: Text = Text()
 
-    def check_password_strength(e) -> None:
-        password: str = str(e.control.value)
-        strength: int = 0
-
-        if len(password) >= 8:
-            strength += 1
-        if re.search(pattern=r"[A-Z]", string=password):
-            strength += 1
-        if re.search(pattern=r"[a-z]", string=password):
-            strength += 1
-        if re.search(pattern=r"\d", string=password):
-            strength += 1
-        if re.search(pattern=r"[!@#$%^&*]", string=password):
-            strength += 1
-
-        print(f"Strength: {strength}")
-
-        if strength <= 2:
-            result_text.value = "Weak 😟"
-            result_text.color = "red"
-        elif strength == 3 or strength == 4:
-            result_text.value = "Midium 🤨"
-            result_text.color = "yellow"
-        else:
-            result_text.value = "Strong 💪"
-            result_text.color = "green"
-        page.update()
-
-    password_input: TextField = TextField(
-        label="Enter Password",
-        password=True,
-        can_reveal_password=True,
-        on_change=check_password_strength,
-        width=300,
+    number_input: TextField = TextField(
+        label="Enter a Number", width=200, keyboard_type=ft.KeyboardType.NUMBER
     )
 
-    page.add(password_input, result_text)
+    result: Column = Column(controls=[])
+
+    def clear_page() -> None:
+        page.controls.clear()
+
+    def generate_table(e) -> None:
+        print("inside generate_table")
+        if number_input.value.isdigit():
+            num = int(number_input.value)
+            print(f"num: {num}")
+            for i in range(1, 11):
+                print(f"i: {i} ")
+                line = f" {num} X {i} = {num * i}"
+                result.controls.append(Text(value=line, size=18))
+            result.controls.append(
+                ElevatedButton(text="Press to clear", on_click=clear_page)
+            )
+        else:
+            result.controls.append(Text(value="Please Enter A Valid Number"))
+        page.update()
+
+    generate_btn = ElevatedButton(
+        text="Press to generate the table", on_click=generate_table
+    )
+
+    page.add(number_input, generate_btn, result)
 
 
 if __name__ == "__main__":
